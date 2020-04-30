a58e7e96012e44692122af3653f21770781112ab
Stéphane PATERNA <paterna@club-internet.fr>
2020-04-30 19:44:31 +0200

    Modifications de bakery-dom.mjs

--- js/dom/bakery-dom.mjs
+++ js/dom/bakery-dom.mjs
@@ -3,10 +3,16 @@ import Bakery from '../classes/bakery.mjs';
 export const updateBakery = (element) => {
     let bakery = document.getElementById('bakery');
     let bakeryName = bakery.getElementsByTagName('h2')[0];
-    console.log(bakeryName);
     bakeryName.innerHTML = element.name;
 
-    
+    let stock = document.getElementById('cookiesStock');
+    let cookiesNumber = stock.getElementsByTagName('span')[0];
+    cookiesNumber.innerHTML = element.cookies;
+
+
+    let cookiesPerSecond = document.getElementById('cookiesPerSecond');
+    let cookiesPerSecondNumber = cookiesPerSecond.getElementsByTagName('span')[0];
+    cookiesPerSecondNumber.innerHTML = element.cookiesPerClick;
 }
 
 
