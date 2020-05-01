643111404558c311b05bcb5393c43eb0cd5de882
Stéphane PATERNA <paterna@club-internet.fr>
2020-05-01 15:56:34 +0200

    Construction des tuiles buildings

--- js/dom/bakery-dom.mjs
+++ js/dom/bakery-dom.mjs
@@ -1,18 +1,15 @@
-import Bakery from '../classes/bakery.mjs';
-
 export const updateBakery = (element) => {
     let bakery = document.getElementById('bakery');
     let bakeryName = bakery.getElementsByTagName('h2')[0];
     bakeryName.innerHTML = element.name;
 
     let stock = document.getElementById('cookiesStock');
-    let cookiesNumber = stock.getElementsByTagName('span')[0];
-    cookiesNumber.innerHTML = element.cookies;
-
+    let cookiesStock = stock.getElementsByTagName('span')[0];
+    cookiesStock.innerHTML = element.cookies;
 
     let cookiesPerSecond = document.getElementById('cookiesPerSecond');
     let cookiesPerSecondNumber = cookiesPerSecond.getElementsByTagName('span')[0];
-    cookiesPerSecondNumber.innerHTML = element.cookiesPerClick;
+    cookiesPerSecondNumber.innerHTML = element.cookiesPerSecond;
 }
 
 
